798ed68e423e9a600224b35232471a26405211fe
sergedmitr <sergey.dmitr@mail.ru>
2019-12-08 12:42:52 +0300

    Add params.

--- src/main/java/ru/sergdm/prospring5/ch2/HelloWorld.java
+++ src/main/java/ru/sergdm/prospring5/ch2/HelloWorld.java
@@ -2,6 +2,10 @@ package ru.sergdm.prospring5.ch2;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		if (args.length > 0) {
+			System.out.println(args[0]);
+		} else {
+			System.out.println("Hello World!");
+		}
 	}
 }
