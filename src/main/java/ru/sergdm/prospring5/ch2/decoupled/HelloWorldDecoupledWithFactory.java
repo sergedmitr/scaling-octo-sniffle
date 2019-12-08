448594fd3cc7040c9787052dbcd4955aa0520da0
sergedmitr <sergey.dmitr@mail.ru>
2019-12-08 13:44:50 +0300

    Factory added.

---
+++ src/main/java/ru/sergdm/prospring5/ch2/decoupled/HelloWorldDecoupledWithFactory.java
@@ -0,0 +1,11 @@
+package ru.sergdm.prospring5.ch2.decoupled;
+
+public class HelloWorldDecoupledWithFactory {
+	public static void main (String... args) {
+		MessageRenderer mr = MessageSupportFactory.getInstance().getMessageRenderer();
+		MessageProvider mp = MessageSupportFactory.getInstance().getMessageProvider();
+
+		mr.setMessageProvider(mp);
+		mr.render();
+	}
+}
